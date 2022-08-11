b902abd676af49f5b6c649cb8e0309da024231c3
Abdul Kayum <70036439+abdulkayum97@users.noreply.github.com>
2022-08-11 15:20:48 +0600

    Add files via upload

--- resourses/js/main.js
+++ resourses/js/main.js
@@ -1,3 +1,44 @@
-function openNav(){document.getElementById("myNav").style.width="100%"}function closeNav(){document.getElementById("myNav").style.width="0%"}$(document).ready(function(){$(".js--services-section").waypoint(function(t){"down"==t?$("nav").addClass("sticky"):$("nav").removeClass("sticky")});mixitup(".container");$("a").on("click",function(t){if(""!==this.hash){t.preventDefault();var n=this.hash;$("html, body").animate({scrollTop:$(n).offset().top},800,function(){window.location.hash=n})}})});
+$(document).ready(function () {
 
-$("svg.radial-progress").each(function(e,t){$(this).find($("circle.complete")).removeAttr("style")}),$(window).scroll(function(){$("svg.radial-progress").each(function(e,t){$(window).scrollTop()>$(this).offset().top-.75*$(window).height()&&$(window).scrollTop()<$(this).offset().top+$(this).height()-.25*$(window).height()&&(percent=$(t).data("percentage"),radius=$(this).find($("circle.complete")).attr("r"),circumference=2*Math.PI*radius,strokeDashOffset=circumference-percent*circumference/100,$(this).find($("circle.complete")).animate({"stroke-dashoffset":strokeDashOffset},1250))})}).trigger("scroll");
+
+    //STICKY MENU
+
+    $(".js--services-section").waypoint(function (direction) {
+
+        if (direction == "down") {
+            $("nav").addClass("sticky");
+        } else {
+            $("nav").removeClass("sticky");
+        }
+    });
+
+
+    //MIX IT UP (PORTFOLIO SECTION)
+    var mixer = mixitup('.container');
+    
+    //SMOTH SCROOL FOR IE OR SUFFARI
+    
+    $("a").on('click', function(event){
+        
+        if (this.hash !== "") {
+            event.preventDefault ();
+            
+            var hash = this.hash;
+            
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+    
+
+});
+
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+    }
+function closeNav(){
+    document.getElementById("myNav").style.width = "0%";
+    }
